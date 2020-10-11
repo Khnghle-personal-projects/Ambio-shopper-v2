e93c1a8177bf79b858e8513ebeeca02acb634d55
Khuong <lekhuongy@gmail.com>
2020-10-11 08:12:21 -0500

    added through table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,23 +1,8 @@
 const User = require('./user')
 const Product = require('./product')
 const Order = require('./order')
-const cartItem = require('./cart')
-const db = require('../db')
 const Cart = require('./cart')
-
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+const db = require('../db')
 
 /*
   User (or anonymous) --> Makes orders (hasMany, belongsToOne)
@@ -30,8 +15,8 @@ User.hasMany(Order)
 Order.belongsTo(User)
 
 //There can be many Products in an order, and a product can be in many orders
-// Order.belongsToMany(Product, {through: Cart})
-// Product.belongsToMany(Order, {through: Cart})
+Order.belongsToMany(Product, {through: Cart})
+Product.belongsToMany(Order, {through: Cart})
 
 module.exports = {
   User,
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,9 +1,22 @@
+// const Sequelize = require('sequelize')
+// const db = require('../db')
+
+// const Order = db.define('cart', {
+//   status: {
+//     type: Sequelize.ENUM(['completed', 'created', 'pending']),
+//     defaultValue: 'created'
+//   }
+// })
+
+// module.exports = Order
+
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Order = db.define('cart', {
+const Order = db.define('order', {
   status: {
-    type: Sequelize.ENUM(['completed', 'created', 'pending']),
+    //What is the purpose of processing
+    type: Sequelize.ENUM('created', 'completed', 'processing'),
     defaultValue: 'created'
   }
 })
