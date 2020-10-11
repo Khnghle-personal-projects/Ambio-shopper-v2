04a217498c5dd24973f98851721d23443c3e9eba
Khuong <lekhuongy@gmail.com>
2020-10-11 09:44:46 -0500

    got rid of old code

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,15 +1,3 @@
-// const Sequelize = require('sequelize')
-// const db = require('../db')
-
-// const Order = db.define('cart', {
-//   status: {
-//     type: Sequelize.ENUM(['completed', 'created', 'pending']),
-//     defaultValue: 'created'
-//   }
-// })
-
-// module.exports = Order
-
 const Sequelize = require('sequelize')
 const db = require('../db')
 
