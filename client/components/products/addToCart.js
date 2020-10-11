471522b7200386cc316d49c5d1250f3e3639cc23
Khuong <lekhuongy@gmail.com>
2020-10-10 22:01:28 -0500

    fixed values for add to cart form

--- client/components/products/addToCart.js
+++ client/components/products/addToCart.js
@@ -31,9 +31,9 @@ class AddToCartForm extends React.Component {
           <option value="4">4</option>
           <option value="5">5</option>
           <option value="6">6</option>
-          <option value="6">7</option>
-          <option value="6">8</option>
-          <option value="6">9</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
         </select>
         <button type="submit">Add to cart</button>
       </form>
