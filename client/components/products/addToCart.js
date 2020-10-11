cf94e65c2d48f48cc8808db4c8656164ceaf7f2a
Khuong <lekhuongy@gmail.com>
2020-10-10 21:47:34 -0500

    addToCart works as a form

--- client/components/products/addToCart.js
+++ client/components/products/addToCart.js
@@ -1,22 +1,44 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-function AddToCartForm(props) {
-  return (
-    <form className="qty-form">
-      <select>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="6">7</option>
-        <option value="6">8</option>
-        <option value="6">9</option>
-      </select>
-      <button type="button">Add to cart</button>
-    </form>
-  )
+class AddToCartForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      qty: 1
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange(evt) {
+    this.setState({[evt.target.name]: evt.target.value})
+  }
+
+  onSubmit(evt) {
+    evt.preventDefault()
+    console.log(
+      `You are trying to buy ${this.state.qty} of ${this.props.item.name}`
+    )
+  }
+  render() {
+    return (
+      <form className="add-to-cart-form" onSubmit={this.onSubmit}>
+        <select name="qty" onChange={this.onChange}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="6">7</option>
+          <option value="6">8</option>
+          <option value="6">9</option>
+        </select>
+        <button type="submit">Add to cart</button>
+      </form>
+    )
+  }
 }
 
-export default AddToCartForm
+export default connect(null, null)(AddToCartForm)
