48799f42467f32b4ebb33388f00b4c9d4feb640a
Khuong <lekhuongy@gmail.com>
2020-10-10 19:21:47 -0500

    added pagination

--- client/components/cart/display.js
+++ client/components/cart/display.js
@@ -4,33 +4,63 @@ import {connect} from 'react-redux'
 class DisplayInfo extends React.Component {
   constructor() {
     super()
+    this.state = {
+      page: 1
+    }
   }
   render() {
     const {products} = this.props
-    return (
-      <div className="item-container">
-        {products.map(curr => (
-          <div key={curr.id} className="single-item">
-            <img src={curr.pic} className="image-display" />
-            <div className="item-name">{curr.name}</div>
-            <div>Price: ${curr.price}</div>
 
-            <form>
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="6">7</option>
-                <option value="6">8</option>
-                <option value="6">9</option>
-              </select>
-              <button type="button">Add to cart</button>
-            </form>
-          </div>
-        ))}
+    const start = (this.state.page - 1) * 8
+    const focus = products.slice(start, start + 8)
+
+    const numPages = Math.ceil(products.length / 8)
+
+    const pageArray = []
+    for (let i = 1; i <= numPages; i++) {
+      pageArray.push(i)
+    }
+
+    return (
+      <div id="all-products-view">
+        <div className="item-container">
+          {focus.map(curr => (
+            <div key={curr.id} className="single-item">
+              <img src={curr.pic} className="image-display" />
+              <div className="item-name">{curr.name}</div>
+              <div>Price: ${curr.price}</div>
+
+              <form>
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="6">7</option>
+                  <option value="6">8</option>
+                  <option value="6">9</option>
+                </select>
+                <button type="button">Add to cart</button>
+              </form>
+            </div>
+          ))}
+        </div>
+
+        <div className="page-numbers">
+          {pageArray.map(curr => {
+            return (
+              <button
+                type="button"
+                key={curr}
+                onClick={() => this.setState({page: curr})}
+              >
+                {curr}
+              </button>
+            )
+          })}
+        </div>
       </div>
     )
   }
