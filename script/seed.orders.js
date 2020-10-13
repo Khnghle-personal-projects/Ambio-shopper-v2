cc85fef2d17a28947d237c28161cbdb718e0d830
Khuong <lekhuongy@gmail.com>
2020-10-13 14:59:20 -0500

    updated seed file

---
+++ script/seed.orders.js
@@ -0,0 +1,20 @@
+const orders = [
+  {
+    status: 'created',
+    userId: 1
+  },
+  {
+    status: 'completed',
+    userId: 1
+  },
+  {
+    status: 'completed',
+    userId: 1
+  },
+  {
+    status: 'processing',
+    userId: 2
+  }
+]
+
+module.exports = orders
