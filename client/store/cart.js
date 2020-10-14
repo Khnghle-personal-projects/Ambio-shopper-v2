6913ce025b39b9cb1af217207a2b8091c60a884a
Khuong <lekhuongy@gmail.com>
2020-10-13 22:25:58 -0500

    made an add item action creator, and thunk within store

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,12 +4,13 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GET_CART = 'GET_CART'
-
+const ADD_ITEM = 'ADD_ITEM'
 /**
  * ACTION CREATORS
  */
 
-export const getCart = cart => ({type: GET_CART, cart})
+const getCart = cart => ({type: GET_CART, cart})
+const addItem = () => ({type: ADD_ITEM})
 
 /**
  * THUNK CREATORS
@@ -25,6 +26,17 @@ export const fetchCart = userId => {
   }
 }
 
+export const updateCart = (userId, orderId, productId, qty) => {
+  return async function(dispatch) {
+    try {
+      await axios.post(`/api/cart/${userId}/${orderId}/${productId}`, {qty})
+      dispatch(addItem())
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
 //INITIAL STATE
 const initialState = []
 
@@ -33,6 +45,8 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_CART:
       return action.cart
+    case ADD_ITEM:
+      return [...state]
     default:
       return state
   }
