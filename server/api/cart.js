ff32c6dd5d81fd797b93b729359e01c3c5c78113
Khuong <lekhuongy@gmail.com>
2020-10-13 19:50:31 -0500

    added get, delete and post route for cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,6 +2,16 @@ const router = require('express').Router()
 const {Cart, Order, Product} = require('../db/models')
 module.exports = router
 
+//Security Middleware
+const userOnly = (req, res, next) => {
+  if (!req.user.id || req.user.id !== req.params.userId) {
+    const err = new Error('Unauthorized User')
+    err.status = 401
+    return next(err)
+  }
+  next()
+}
+
 //Grab a current cart for the user (or create a new one)
 //Products can be found within products key
 router.get('/:userId', async (req, res, next) => {
@@ -24,5 +34,54 @@ router.get('/:userId', async (req, res, next) => {
     res.send(cart)
   } catch (err) {
     next(err)
+  }
+})
+
+//Grabs an item from the cart
+router.get('/:userId/:orderId/:productId', async (req, res, next) => {
+  try {
+    const item = await Cart.findOne({
+      where: {
+        orderId: req.params.orderId,
+        productId: req.params.productId
+      }
+    })
+    res.send(item)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Deletes an item from the cart
+router.delete('/:userId/:orderId/:productId', async (req, res, next) => {
+  try {
+    await Cart.destroy({
+      where: {
+        orderId: req.params.orderId,
+        productId: req.params.productId
+      }
+    })
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Adds an item to the cart
+router.post('/:userId/:orderId/:productId', async (req, res, next) => {
+  try {
+    const [item] = await Cart.findOrCreate({
+      where: {
+        orderId: req.params.orderId,
+        productId: req.params.productId
+      }
+    })
+
+    item.qty = Number(req.body.qty)
+    await item.save()
+
+    res.send(item)
+  } catch (error) {
+    next(error)
   }
 })
